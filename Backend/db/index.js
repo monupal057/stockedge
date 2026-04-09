632869d37c9b3d376d2fef90e8e37336e15d3d8b
Monu pal <76943034+monupal057@users.noreply.github.com>
2026-04-09 13:37:56 +0530

    update

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,5 +1,36 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "../constants.js";
+
+// let isConnected = false;
+
+// const connectDB = async () => {
+//     if (isConnected) {
+//         console.log("♻️ Reusing existing MongoDB connection");
+//         return;
+//     }
+
+//     try {
+//         const conn = await mongoose.connect(
+//             // `${process.env.MONGODB_URI}/${DB_NAME}`,
+//             process.env.MONGO_URI
+//             {
+//                 maxPoolSize: 10,
+//                 serverSelectionTimeoutMS: 5000,
+//                 socketTimeoutMS: 45000,
+//             }
+//         );
+//         isConnected = conn.connections[0].readyState === 1;
+//         console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+//     } catch (error) {
+//         console.error("❎ MongoDB connection FAILED", error);
+//         process.exit(1);
+//     }
+// };
+
+// export default connectDB;
+
+
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 let isConnected = false;
 
@@ -11,13 +42,14 @@ const connectDB = async () => {
 
     try {
         const conn = await mongoose.connect(
-            `${process.env.MONGODB_URI}/${DB_NAME}`,
+            process.env.MONGO_URI,
             {
                 maxPoolSize: 10,
                 serverSelectionTimeoutMS: 5000,
                 socketTimeoutMS: 45000,
             }
         );
+
         isConnected = conn.connections[0].readyState === 1;
         console.log(`✅ MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
